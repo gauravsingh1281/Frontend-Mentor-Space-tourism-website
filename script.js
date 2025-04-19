9711408c550676ff03dd2d75d22e452b1ad611d7
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-19 14:52:20 +0530

    mobile nav completed.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const showMobileNavBtn = document.getElementById("mobile-nav-show-btn");
 const closeMobileNavBtn = document.getElementById("mobile-nav-close-btn");
 const mobileNav = document.querySelector(".mobile-nav-link ");
+const exploreBtn = document.getElementById("explore-btn");
+const explorebtnOuterArea = document.getElementById("explore-btn-outer");
 
 showMobileNavBtn.addEventListener("click", () => {
-  mobileNav.classList.remove("hidden");
-  mobileNav.classList.add("flex");
+  mobileNav.style.right = "0";
 });
 closeMobileNavBtn.addEventListener("click", () => {
-  mobileNav.classList.add("hidden");
-  mobileNav.classList.remove("flex");
+  mobileNav.style.right = "-100%";
+});
+
+exploreBtn.addEventListener("mouseover", () => {
+  explorebtnOuterArea.style.opacity = 0.3;
+});
+exploreBtn.addEventListener("mouseleave", () => {
+  explorebtnOuterArea.style.opacity = 0;
 });
